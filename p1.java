6d7878ed643368542b7d60a4ea38370640bb2f08
suks-j <ljinnannavar@gmail.com>
2026-04-17 22:15:12 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,27 @@
+package lab1b;
+import java.util.ArrayList;
+public class p1 {
+	
+		    public static void main(String[] args) {
+
+		        // Creating ArrayList
+		        ArrayList<String> colors = new ArrayList<>();
+
+		        // Adding colors
+		        colors.add("Blue");
+		        colors.add("Green");
+		        colors.add("Yellow");
+		        colors.add("Red");
+		        colors.add("White");
+
+		        // Display colors
+		        System.out.println("List of colors: " + colors);
+
+		        // Searching for "Red"
+		        if (colors.contains("Red")) {
+		            System.out.println("Red color is available in the list.");
+		        } else {
+		            System.out.println("Red color is NOT available in the list.");
+		        }
+		    }
+		}    
