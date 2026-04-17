4b84c58d34fadb19685227959e2a3427f27e9532
suks-j <ljinnannavar@gmail.com>
2026-04-17 21:43:24 +0530

    Update HelloSwing.java

--- HelloSwing.java
+++ HelloSwing.java
@@ -1,3 +1,6 @@
+/*4a. Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing Programming
+in Blue color plain font with font size of 32 using Jframe and Jlabel*/
+
 package lab4a;
 import javax.swing.*;
 import java.awt.*;
