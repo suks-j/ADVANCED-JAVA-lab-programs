dc74c3267be5c7b7d1b3c227bc0dce15d5e2fe91
suks-j <ljinnannavar@gmail.com>
2026-04-18 19:07:36 +0530

    Add files via upload

---
+++ p19.java
@@ -0,0 +1,20 @@
+package lab3c;
+import java.util.Random;
+
+public class p19 {
+
+    public static String generateRandomString(int length) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder result = new StringBuilder();
+        Random rand = new Random();
+
+        for (int i = 0; i < length; i++) {
+            result.append(chars.charAt(rand.nextInt(chars.length())));
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateRandomString(8));
+    }
+}
